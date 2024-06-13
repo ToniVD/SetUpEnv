8386857bb236ffd607c8b35bbc7bda94aafd7ee8
ToniVD <vividrakes2147@gmail.com>
2024-06-13 15:09:35 -0400

    functions practice task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -14,4 +14,12 @@ let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function groceryTracker(){
+grocery1= parseFloat(document.getElementById('grocery1').value);
+grocery2= parseFloat(document.getElementById('grocery2').value);
+grocery3= parseFloat(document.getElementById('grocery3').value);
 
+let expenditure = grocery1 + grocery2 + grocery3;
+
+document.getElementById('total').innerText=`The total expenditure is: ${expenditure}`;
+}
